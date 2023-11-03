43850cd2966ef048bf12132b8e8995bb70127da3
Josh Febbie <joshdfebbie@gmail.com>
2023-11-02 21:13:38 -0700

    2nd_Sub-hero

---
+++ components/Main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { FaLinkedinIn} from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa'
+import { AiOutlineMail } from 'react-icons/ai'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+
+
+const Main = () => {
+  return (
+    <div className='w-full h-screen text-center'>
+        <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center text-lg'>
+            <div>
+                <p className='uppercase text-sm tracking-widest text-gray-600'>Let's build something great!</p>
+                <h1 className='py-4 text-gray-700'>
+                    Hi, I'm<span className='text-[#398378]'> Josh</span>
+                </h1>
+                <h1 className="py-2 text-gray-600">
+                    A Full-Stack Web Developer
+                </h1>
+                <p className='py-4  text-gray-600 max-w-[70%] m-auto'>
+                    I'm a full-stack web developer who enjoys building things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide pixel-perfect, performant experiences.
+                </p>
+                <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                    <div className='p-5 bg-[#398378] text-white rounded-full shadow-lg shadow-gray-500 hover:bg-[#3fa38c] hover:scale-110 transition duration-500 ease-in-out cursor-pointer'>
+                        <FaLinkedinIn/>
+                    </div>
+                    <div className='p-5 bg-[#398378] text-white rounded-full shadow-lg shadow-gray-500 hover:bg-[#3fa38c] hover:scale-110 transition duration-500 ease-in-out cursor-pointer'>
+                        <FaGithub />
+                    </div>
+                    <div className='p-5 bg-[#398378] text-white rounded-full shadow-lg shadow-gray-500 hover:bg-[#3fa38c] hover:scale-110 transition duration-500 ease-in-out cursor-pointer'>
+                        <AiOutlineMail />
+                    </div>
+                    <div className='p-5 bg-[#398378] text-white rounded-full shadow-lg shadow-gray-500 hover:bg-[#3fa38c] hover:scale-110 transition duration-500 ease-in-out cursor-pointer'>
+                        <BsFillPersonLinesFill />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Main
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from "react-icons/ai";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
@@ -31,19 +39,34 @@ const Navbar = () => {
               </li>
             </Link>
           </ul>
-          <div className="md:hidden">
+          <div onClick={handleNav} className="md:hidden cursor-pointer">
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
 
-      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
-        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+      <div
+        className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""}
+      >
+        <div
+          className={
+            nav
+              ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-700"
+              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+          }
+        >
           <div>
             <div className="flex w-full items-center justify-between">
-              <Image src="/assets/logo2.png" width="90" height="30" alt="/" />
-              <div className="rounded-full shadow-lg shadow-gray-500 p-3 cursor-pointer">
-                <AiOutlineClose size={25} />
+              <Image
+                src="/assets/logo2.png"
+                width="90"
+                height="30"
+                alt="/" />
+              <div
+                onClick={handleNav}
+                className="rounded-full shadow-lg shadow-gray-500 p-3 cursor-pointer"
+              >
+                <AiOutlineClose size={20} />
               </div>
             </div>
             <div className="border-b border-gray-400 my-4">
@@ -71,13 +94,22 @@ const Navbar = () => {
               </Link>
             </ul>
             <div className="pt-40">
-              <p className="uppercase tracking-widest ">Let's Connect</p>
-              <div>
-                <div>
+              <p className="uppercase tracking-widest" id="connect">
+                Let's Connect
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <div className="p-3 bg-[#398378] text-white rounded-full shadow-lg shadow-gray-500 hover:bg-[#3fa38c] hover:scale-110 transition duration-500 ease-in-out cursor-pointer">
                   <FaLinkedinIn />
                 </div>
-
-
+                <div className="p-3 bg-[#398378] text-white rounded-full shadow-lg shadow-gray-500 hover:bg-[#3fa38c] hover:scale-110 transition duration-500 ease-in-out cursor-pointer">
+                  <FaGithub />
+                </div>
+                <div className="p-3 bg-[#398378] text-white rounded-full shadow-lg shadow-gray-500 hover:bg-[#3fa38c] hover:scale-110 transition duration-500 ease-in-out cursor-pointer">
+                  <AiOutlineMail />
+                </div>
+                <div className="p-3 bg-[#398378] text-white rounded-full shadow-lg shadow-gray-500 hover:bg-[#3fa38c] hover:scale-110 transition duration-500 ease-in-out cursor-pointer">
+                  <BsFillPersonLinesFill />
+                </div>
               </div>
             </div>
           </div>
